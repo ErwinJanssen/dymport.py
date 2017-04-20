a9bb32b91e2b742705b6292bd52fc869a8130766
Erwin Janssen <erwinjanssen@outlook.com>
2017-04-20 18:25:51 +0200

    Add check for supported Python versions
    Not all Python versions are supported by this package, because the import
    mechanism changes in the different Python versions.
    If an unsupported Python version is used, an ImportError is raised.

--- dymport/import_file.py
+++ dymport/import_file.py
@@ -4,7 +4,7 @@ Various functions to dynamically import (abitrary names from) arbitrary files.
 To import a file like it is a module, use `import_file`.
 """
 
-from importlib.util import module_from_spec, spec_from_file_location
+from sys import version_info
 
 
 def import_file(name, file):
@@ -13,11 +13,18 @@ def import_file(name, file):
 
     Raises an ImportError if it could not be imported.
     """
-    spec = spec_from_file_location(name, file)
+    if version_info > (3, 5):
+        from importlib.util import module_from_spec, spec_from_file_location
 
-    if not spec:
-        raise ImportError("Could not import: '{}'".format(file))
+        spec = spec_from_file_location(name, file)
 
-    module = module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
+        if not spec:
+            raise ImportError("Could not import: '{}'".format(file))
+
+        module = module_from_spec(spec)
+        spec.loader.exec_module(module)
+        return module
+
+    else:
+        raise ImportError("Dymport: unsupported Python version, could not "
+                          "import '{}'".format(file))
